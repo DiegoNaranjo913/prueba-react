941129de89d2b5e66617e30737711f954459a14c
diego <diegofernando913@gmail.com>
2020-12-04 03:25:43 -0500

    se hace el fadeout
    se hace el fadeout

--- src/components/form.js
+++ src/components/form.js
@@ -2,19 +2,52 @@ import React from 'react';
 import '../styles/form.scss';
 import { useForm } from 'react-hook-form'
 import axios from 'axios';
+import { useSelector } from 'react-redux';
+
+const URL = "https://murmuring-peak-43417.herokuapp.com/api/users";
 
 
 export default function Form() {
 
     const { register, handleSubmit, errors } = useForm();
+    const page = useSelector(state => state.page);
+
 
     const Submit = (data, e) => {
-        e.target.reset();
-        console.log(data);
-        console.log("buenas");
-        var x = document.getElementById("div_loader");
-        x.style.display = "flex";
+        let loader = document.getElementById("div_loader");
+        let box = document.getElementById("fade-out");
+        let message = document.getElementById("text");
+        loader.style.display = "flex";
+        axios.post(URL, { "name": data.name, "email": data.email, "phone": data.phone, "nickname": data.email.split("@")[0], "vehicle": page, "age": data.age }).then(res => {
+            loader.style.display = "none";
+            if (res.data === "Usuario Existente") {
+                box.style.backgroundColor = "red"
+            } else {
+                box.style.backgroundColor = "green"
+            }
+            e.target.reset();
+            message.textContent = res.data;
+            fadeOut(box);
+        });
     }
+
+    const fadeOut = (elem) => {
+        let newValue = 1;
+        elem.style.opacity = 1;
+        let fadeOutInterval = setInterval(function () {
+            if (newValue > 0) {
+                newValue -= 0.2;
+            } else if (newValue < 0) {
+                elem.style.opacity = 0;
+                clearInterval(fadeOutInterval);
+            }
+
+            elem.style.opacity = newValue;
+
+        }, 1000);
+
+    }
+
 
 
 
@@ -29,10 +62,10 @@ export default function Form() {
                         name="name"
                         ref={
                             register({
-                                required: "El campo nombre no debe ir vacío",
+                                required: "El campo nombre no debe ir vacío.",
                                 pattern: {
-                                    value: /[A-Za-z ñ]+/,
-                                    message: "El campo debe ser solo texto"
+                                    value: /^[\u00F1A-Za-z _]*[\u00F1A-Za-z][\u00F1A-Za-z _]*$/,
+                                    message: "El nombre digitado no es valido."
                                 }
                             })
                         }
@@ -43,9 +76,11 @@ export default function Form() {
                         name="email"
                         ref={
                             register({
-                                required: "El campo email no debe ir vacío",
-                                // pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                                message: "El email no tiene la estructura correcta"
+                                required: "El campo email no debe ir vacío.",
+                                pattern: {
+                                    value: /\S+@\S+\.\S+/,
+                                    message: "El email digitado no es valido."
+                                }
                             })
                         }
                     />
@@ -55,10 +90,10 @@ export default function Form() {
                         name="phone"
                         ref={
                             register({
-                                required: "El campo celular no debe ir vacío",
+                                required: "El campo celular no debe ir vacío.",
                                 pattern: {
                                     value: /^3[\d]{9}$/,
-                                    message: "El número del celular no es valido"
+                                    message: "El número del celular no es valido."
                                 },
                             })
                         }
@@ -69,18 +104,18 @@ export default function Form() {
                         name="age"
                         ref={
                             register({
-                                required: "El campo edad no debe ir vacío",
-                                // pattern: {
-                                //     value: /^[0 - 9]$/,
-                                //     message: "La edad debe ser un número"
-                                // },
+                                required: "El campo edad no debe ir vacío.",
+                                pattern: {
+                                    value: /^[0-9.]+$/,
+                                    message: "La edad digitada no es valida."
+                                },
                                 min: {
                                     value: 18,
-                                    message: "La edad minima es de 18"
+                                    message: "La edad minima es de 18."
                                 },
                                 max: {
                                     value: 100,
-                                    message: "La edad maxima es de 100"
+                                    message: "La edad maxima es de 100."
                                 },
                             })
                         }
@@ -94,10 +129,8 @@ export default function Form() {
                     </div>
                 </form>
             </div>
-            <div className="div_loader">
-                <div class="loader">
-
-                </div>
+            <div className="fade-out" id="fade-out">
+                <p id="text"></p>
             </div>
         </div>
     );
